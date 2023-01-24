690a954b1a30cdee1468d31429c747db520b44d5
MD Anamul Islam <114305511+mdanamulislam@users.noreply.github.com>
2023-01-25 00:17:06 +0600

    Add files via upload

--- problem_13.js
+++ problem_13.js
@@ -1,7 +1,12 @@
 //////////sovled//////////
 
 function sumPolygon(n){
-   return (n-2) * 180;
+   if(n > 2){
+      return (n-2) * 180;
+   }
+   else{
+      return "this number always greater than 2";
+   }
 }
 
-console.log(sumPolygon(6));
+console.log(sumPolygon(3));
